3dd385597ba27cac859fe026746009be7b243424
raghavendral <raghavendral@DESKTOP-NPP3JK4>
2024-03-29 20:54:24 +0530

    I have added a test script Before method and after

--- DemoWebShop/src/test/java/baseclass/BaseClassn.java
+++ DemoWebShop/src/test/java/baseclass/BaseClassn.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,18 +13,30 @@ public class BaseClassn {
   public static WebDriver driver;
   
   @BeforeClass
-    public void lanch() {
+    public void launch() {
 	  driver = new ChromeDriver();
 	  driver.get("https://demowebshop.tricentis.com/");
 	  driver.manage().window().maximize();
 	 
  }
   
+ @BeforeMethod
+ public void logindetails() {
+	 driver.findElement(By.linkText("Log in")).click();
+	 driver.findElement(By.id("Email")).sendKeys("madhu673gmail.com");
+	 driver.findElement(By.id("Password")).sendKeys("madhu673");
+	 driver.findElement(By.linkText("Log in")).click();
+ }
   
-    
-  @AfterClass
+   @AfterMethod
   public void logout() {
 	  driver.findElement(By.xpath("//a[text()=\"Log out\"]")).click();
   }
 
+  @AfterClass
+  public void close() {
+	  driver.quit();
+  }
+   
+   
 }
